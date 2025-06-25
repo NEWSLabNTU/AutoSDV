12c5e96f591113613b189e2b7433450884ad4d2b
aeon <jerry73204@gmail.com>
2025-06-25 21:43:58 +0800

    Update setup-zenoh.sh to allow multicast and scouting

--- scripts/setup-zenoh/setup-zenoh.sh
+++ scripts/setup-zenoh/setup-zenoh.sh
@@ -5,6 +5,7 @@ cd "$script_dir"
 
 export AUTOWARE_HOME=/opt/autoware
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+export ZENOH_CONFIG_OVERRIDE='listen/endpoints=["tcp/0.0.0.0:0"];scouting/multicast/enabled=true'
 
 # sudo apt install ros-humble-rmw-zenoh-cpp
 pkill -9 -f ros && ros2 daemon stop
