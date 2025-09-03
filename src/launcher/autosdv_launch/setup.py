a4fa9af35c5f57bfacb067ddf2be50d212604802
aeon <jerry73204@gmail.com>
2025-09-04 05:41:03 +0800

    Move system monitor code to autosdv_system_monitor submodule.

--- src/launcher/autosdv_launch/setup.py
+++ src/launcher/autosdv_launch/setup.py
@@ -16,15 +16,6 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*launch.[pxy][ymal]*")),
         ),
-        (
-            os.path.join("share", package_name, "config"),
-            glob(os.path.join("config", "*.yaml")),
-        ),
-        # Include the HTML templates
-        (
-            os.path.join("share", package_name, "autosdv_launch", "templates"),
-            glob(os.path.join("autosdv_launch", "templates", "*.html")),
-        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -34,8 +25,6 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [
-            "autosdv_monitor = autosdv_launch.autosdv_monitor:main",
-        ],
+        "console_scripts": [],
     },
 )
