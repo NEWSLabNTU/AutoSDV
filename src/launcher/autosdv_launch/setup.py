a321c6372d98d01e9ac174e96edb31c45ac7f176
aeon <jerry73204@gmail.com>
2025-09-05 07:38:33 +0800

    Add customized RViz config.

--- src/launcher/autosdv_launch/setup.py
+++ src/launcher/autosdv_launch/setup.py
@@ -16,6 +16,10 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*launch.[pxy][ymal]*")),
         ),
+        (
+            os.path.join("share", package_name, "rviz"),
+            glob(os.path.join("rviz", "*.rviz")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
